0cde82345983535ac697047ab1332d1e71a91246
antoine.savajols <antoine.savajols@elosi.com>
2018-03-08 15:25:47 +0100

    ajout routes

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,43 @@
-const Hapi = require('hapi');
+import * as Hapi from 'hapi';
 
-// Create a server with a host and port
-const server = Hapi.server({
-    host: 'localhost',
-    port: 8000
-});
+const server = Hapi.server({ port: 3000, host: 'localhost' });
 
-// Add the route
 server.route({
     method: 'GET',
-    path: '/hello',
+    path: '/',
     handler: (request, h) => {
-        return 'hello world';
+        return 'Hello, world!';
     }
 });
 
-// Start the server
-async function start() {
-    try {
-        await server.start();
-    } catch (err) {
-        console.log(err);
-        process.exit(1);
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: (request, h) => {
+        // request.log(['a', 'name'], "Request name");
+        // or
+        request.logger.info('In handler %s', request.path);
+
+        return `Hello, ${encodeURIComponent(request.params.name)}!`;
     }
+});
 
-    console.log('Server running at:', server.info.uri);
-}
+const init = async () => {
+    await server.register({
+        plugin: require('hapi-pino'),
+        options: {
+            prettyPrint: false,
+            logEvents: ['response']
+        }
+    });
 
-start();
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', err => {
+    console.log(err);
+    process.exit(1);
+});
+
+init();
