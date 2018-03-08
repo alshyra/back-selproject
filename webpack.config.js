b8da305fcb66a23b3642bdd8e33e0061f6f8c261
antoine.savajols <antoine.savajols@elosi.com>
2018-03-08 15:03:52 +0100

    init back end

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+var nodeModules = {};
+
+fs
+    .readdirSync('node_modules')
+    .filter(function(x) {
+        return ['.bin'].indexOf(x) === -1;
+    })
+    .forEach(function(mod) {
+        nodeModules[mod] = 'commonjs ' + mod;
+    });
+
+module.exports = {
+    entry: './src/server.ts',
+    target: 'node',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.ts$/,
+                loader: 'tslint-loader'
+            },
+            {
+                test: /\.tsx?$/,
+                exclude: path.resolve(__dirname, './node_modules'),
+                loader: 'ts-loader'
+            }
+        ]
+    },
+    externals: nodeModules
+};
